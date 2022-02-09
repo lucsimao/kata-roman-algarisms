270c92ab55ec75ead2d5e5cc1e4609068404f747
lucsimao <lucasimao64@gmail.com>
2022-02-09 00:51:11 -0300

    feat: ensure return IV when number 4 is provided

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 export class RomanNumeralsManager {
   public convertDecimalToRoman(number: number) {
     let result = '';
+
+    if (number === 4) return 'IV';
+
     for (let i = 0; i < number; i++) {
       result = result.concat('I');
     }
