f8d3df698794e8249eaef51d7cbc3f5d1d751e0d
lucsimao <lucasimao64@gmail.com>
2022-02-09 01:00:38 -0300

    refactor: use a array to map romanNumerals

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
+const romanNumeralList = ['I', 'II', 'III', 'IV', 'V'];
+
 export class RomanNumeralsManager {
   public convertDecimalToRoman(number: number) {
-    let result = '';
-
-    if (number === 4) return 'IV';
-    if (number === 5) return 'V';
-
-    for (let i = 0; i < number; i++) {
-      result = result.concat('I');
-    }
-
-    return result;
+    return romanNumeralList[number - 1];
   }
 }
